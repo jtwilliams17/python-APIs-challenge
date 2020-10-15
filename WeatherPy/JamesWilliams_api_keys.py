e114ea46b2d43e82f6e19c9b6bec6459a7ebe158
James Williams <jameswilliams@Jamess-MacBook-Pro.local>
2020-10-15 18:39:07 -0400

    remove api keys

--- WeatherPy/JamesWilliams_api_keys.py
+++ WeatherPy/JamesWilliams_api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "7791aa4fb9f16089f78777865ab9c2c5"
+weather_api_key = "Your Key Here"
 
 # Google API Key
-g_key = "AIzaSyAaoNeg0eOaJFoyzYDDN9ldj2B5Lcw4nAg"
+g_key = "Your Key Here"
