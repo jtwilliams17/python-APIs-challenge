7baa35fb971e4c2e219cf9bdd823ce17563b7aa6
James Williams <jameswilliams@Jamess-MacBook-Pro.local>
2020-10-15 18:04:39 -0400

    update 1

--- WeatherPy/JamesWilliams_api_keys.py
+++ WeatherPy/JamesWilliams_api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7791aa4fb9f16089f78777865ab9c2c5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAaoNeg0eOaJFoyzYDDN9ldj2B5Lcw4nAg"
